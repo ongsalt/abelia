cf823e28e38ebcc3026a877a788310836403fe2a
ongsalt <me@ongsa.lt>
2026-06-18 18:39:48 +0700

    remove fd based image loading

--- Sources/CSTBImage/include/stbi_extras.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "stb_image.h"
-
-// Load from a POSIX file descriptor. Closes fd after loading.
-STBIDEF stbi_uc* stbi_load_from_fd(int fd, int* x, int* y, int* channels_in_file, int desired_channels);
--- Sources/CSTBImage/stb_image.c
+++ Sources/CSTBImage/stb_image.c
@@ -1,19 +1,2 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#include "stbi_extras.h"
-#include <stdio.h>
-
-#ifdef _WIN32
-#  include <io.h>
-#  define stbi__fdopen(fd, mode) _fdopen(fd, mode)
-#else
-#  define stbi__fdopen(fd, mode) fdopen(fd, mode)
-#endif
-
-stbi_uc* stbi_load_from_fd(int fd, int* x, int* y, int* channels_in_file, int desired_channels) {
-    FILE* f = stbi__fdopen(fd, "rb");
-    if (!f) return NULL;
-    stbi_uc* result = stbi_load_from_file(f, x, y, channels_in_file, desired_channels);
-    fclose(f);
-    return result;
-}
+#include "include/stb_image.h"
