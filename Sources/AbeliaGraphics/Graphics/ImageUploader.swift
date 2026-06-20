6cd951cd9370884b7be5e9f5b6f228da755d7198
ongsalt <me@ongsa.lt>
2026-06-21 02:45:26 +0700

    fix imported image coloring

--- Sources/AbeliaGraphics/Graphics/ImageUploader.swift
+++ Sources/AbeliaGraphics/Graphics/ImageUploader.swift
@@ -24,7 +24,7 @@ extension TextureRegistry {
         // --- GPU-local image ---
         let texture = try createRenderTexture(
             size: SIMD2(width, height),
-            format: .r8g8b8a8Srgb,
+            format: .r8g8b8a8Unorm,
             usages: [.sampled, .transferDst]
         )
 
